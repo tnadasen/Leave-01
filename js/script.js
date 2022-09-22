d50b0d37d537986a92bfb54dc6cc0dd8a1e4941e
tnadasen <47588765+tnadasen@users.noreply.github.com>
2022-09-22 09:31:13 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// $('#myModal').modal('show')
+
+// const showModal = () => {
+//     // console.log('showModal from the script file')
+//     $('#exampleModalScrollable').modal('show')
+// }
+showModal = () => {
+    // console.log('showModal from the script file')
+    $('#leaveForm').modal('show')
+}
+closeModal = () => {
+    // console.log('showModal from the script file')
+    $('#leaveForm').modal('hide')
+}
+
+showModal2 = () => {
+    // console.log('showModal from the script file')
+    $('#modal2').modal('show')
+}
+closeModal2 = () => {
+    // console.log('showModal from the script file')
+    $('#modal2').modal('hide')
+}
+
+submitRequest = () => {
+    closeModal();
+    setTimeout(() => { alert('Your request has been submitted and pending approval \n ¯\\_(ツ)_/¯') }, 500);
+
+    // alert('your request has been submitted and pending approval')
+    // setTimeout(() => { closeModal(); }, 500);
+}
